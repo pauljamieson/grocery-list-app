56e9c80e818730c84092409b4ef9a0efd87d99fb
Paul Jamieson <36163469+pauljamieson@users.noreply.github.com>
2022-07-12 13:18:53 -0400

    update client

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  console.log("Path:", req.path);
+  console.log("Path:", req.method, req.path);
   next();
 });
 
@@ -25,7 +25,7 @@ app.use("/api", apiRouter);
 
 // Respond with react
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
 app.get("*", (req, res) => {
